9dbf28d4c249835edac838c2e77b795d9663e3cf
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-10-13 23:59:14 +0600

    second commit with responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,12 +5,23 @@ import bgimage from '../../src/assets/bg_image.jpg'
 const HeroSection = () => {
   return (
     <div 
-    className='lg:h-[40em] md:h-[44em] h-[36em]'
-    style={{
-        background: "linear-gradient(80deg, black, transparent), url(https://preview.redd.it/how-can-someone-make-this-background-with-html-and-css-i-v0-zjgs096khv591.jpg?width=1080&crop=smart&auto=webp&s=5ec8b5f14222e27eecd19b9f5428793bcc857b58)"
-    }}>
-      <Navbar/>
-      Hero
+        className='lg:h-[40em] md:h-[44em] h-[36em]'
+        style={{
+            background: `linear-gradient(80deg, black, transparent),url(${bgimage})`,
+            backgroundSize:'cover'
+        }}>
+        <Navbar/>
+        <div>
+          <div className=''>
+            <h1 className='text-white'>Unlimited movies, TV shows, and more</h1>
+            <p className='text-white'>Starts at USD 2.99. Cancel anytime.</p>
+            <p  className='text-white'>Ready to watch? Enter your email to create or restart your membership.</p>
+          </div>
+          <div>
+            <input type="" placeholder='Email address'/>
+            <button className='text-white'>Get Started</button>
+          </div>
+        </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,25 @@ import logo from '../../src/assets/netflix-logo.png'
 const Navbar = () => {
   return (
     <div>
-      <div className='main'>
+      <div className='main flex justify-between lg:justify-around md:justify-around py-2 px-4'>
         <div className='left'>
           <div className='netflix-logo'>
-            <img src={logo} />
+            <img src={logo} className='lg:w-40 md:40 w-32'/>
           </div>
         </div>
-        <div className="right">
-          <div>
-            <select name='' id='' className=''>
-              <option value="Englis">Englis</option>
-              <option value="Bangla">Bangla</option>
+
+        <div className="right flex items-center justify-center">
+          <div className='selector bg-[#393946a1] text-white mx-4 px-2 py-1 rounded-md border-[1px] border-gray-500 hover:ring-2 hover:ring-gray-100'>
+            <select 
+            name='' 
+            id='' 
+            className='bg-transparent outline-none cursor-pointer'>
+              <option value="English"  className='text-[#111111]'>English</option>
+              <option value="Bangla" className='text-[#111111]'>Bangla</option>
             </select>
           </div>
+          
+          <button className='text-white bg-[#ee3636] px-2 py-0.5 rounded-md hover:bg-[#d82626]'>Sign in</button>
         </div>
       </div>
     </div>
