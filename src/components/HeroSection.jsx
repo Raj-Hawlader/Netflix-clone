c29dbe4900f10941b16c198d309e00b393df086c
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-10-14 23:59:09 +0600

    second page

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,25 +4,31 @@ import bgimage from '../../src/assets/bg_image.jpg'
 
 const HeroSection = () => {
   return (
+    <>
     <div 
         className='lg:h-[40em] md:h-[44em] h-[36em]'
         style={{
-            background: `linear-gradient(80deg, black, transparent),url(${bgimage})`,
+            background: `linear-gradient(rgba(0,0,0,.8), rgba(0,0,0,.6)),url(${bgimage})`,
             backgroundSize:'cover'
         }}>
         <Navbar/>
-        <div>
-          <div className=''>
-            <h1 className='text-white'>Unlimited movies, TV shows, and more</h1>
-            <p className='text-white'>Starts at USD 2.99. Cancel anytime.</p>
-            <p  className='text-white'>Ready to watch? Enter your email to create or restart your membership.</p>
-          </div>
+        <div  className='container flex items-center justify-center mt-[7em]'>
           <div>
-            <input type="" placeholder='Email address'/>
-            <button className='text-white'>Get Started</button>
+            <h1 className='text-white text-center font-bold lg:text-5xl md:text-5xl text-3xl'>Unlimited movies, TV <br /> shows, and more</h1>
+            <p className='text-white text-center mt-[1.2em] font-bold text-lg'>Starts at USD 2.99. Cancel anytime.</p>
+            <p className='text-white text-lg mt-[1.2em] text-center'>Ready to watch? Enter your email to create or restart your membership.</p>
+            
+            <div className='flex flex-wrap items-center justify-center m-4 lg:mt-[1.2em] md:mt-[1.2em]'>
+              <input type="text" placeholder='Email address' className='text-white border border-gray-200/50 text-xl bg-transparent px-9 py-3 w-96 rounded-md' />
+              <button className='text-white px-11 py-3 bg-red-600 rounded-md text-xl font-bold ml-4'>Get Started</button>
+            </div>
           </div>
+            
         </div>
     </div>
+
+    <div className='hr_line'/>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import logo from '../../src/assets/netflix-logo.png'
 const Navbar = () => {
   return (
     <div>
-      <div className='main flex justify-between lg:justify-around md:justify-around py-2 px-4'>
+      <div className='main lg:flex md:flex justify-start lg:justify-around md:justify-around py-2 px-4'>
         <div className='left'>
           <div className='netflix-logo'>
-            <img src={logo} className='lg:w-40 md:40 w-32'/>
+            <img src={logo} className='lg:w-40 md:40 w-24'/>
           </div>
         </div>
 
-        <div className="right flex items-center justify-center">
+        <div className="right flex-wrap lg:flex md:flex items-center justify-center">
           <div className='selector bg-[#393946a1] text-white mx-4 px-2 py-1 rounded-md border-[1px] border-gray-500 hover:ring-2 hover:ring-gray-100'>
             <select 
             name='' 
@@ -20,7 +20,7 @@ const Navbar = () => {
               <option value="Bangla" className='text-[#111111]'>Bangla</option>
             </select>
           </div>
-          
+      
           <button className='text-white bg-[#ee3636] px-2 py-0.5 rounded-md hover:bg-[#d82626]'>Sign in</button>
         </div>
       </div>
